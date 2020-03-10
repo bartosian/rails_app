3f337910189ebd601151c38c59bbb0db45c532d9
bartosian <bartosian1989@gmail.com>
2020-03-10 14:34:38 -0400

    Implemented users model

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '~> 5.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '1.3.13'
 gem 'bootstrap-sass', '3.3.6'
+gem 'rails-controller-testing'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
+  get '/signup', to: 'users#new'
+
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
