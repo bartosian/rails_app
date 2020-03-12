e99fd9588d8affb3e988f68ead1cde67b7e85051
bartosian <bartosian1989@gmail.com>
2020-03-12 08:11:09 -0400

    Added sessions support

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
-  resources :users
-  get '/signup', to: 'users#new'
 
-  get '/about', to: 'static_pages#about'
-  get '/help', to: 'static_pages#help'
-  get '/contact', to: 'static_pages#contact'
+  root   'static_pages#home'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  get    '/help',    to:  'static_pages#help'
+
+  get    '/about',   to: 'static_pages#about'
+
+  get    '/contact', to: 'static_pages#contact'
+
+  get    '/signup',  to: 'users#new'
+
   post '/signup', to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+
+  post   '/login',   to: 'sessions#create'
+
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+
 end
